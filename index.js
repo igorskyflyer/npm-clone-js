54b9a3377dfcef6f13b2fcb449866e15bf20408d
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-16 21:49:26 +0200

    Refactor

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
 'use strict'
 
 function isArray(object) {
-	return object instanceof Array
+  return object instanceof Array
 }
 
 function isObject(object) {
-	return typeof object === 'object'
+  return typeof object === 'object'
 }
 
 function isNull(object) {
-	return object === null
+  return object === null
 }
 
 /**
  * Clones the provided object.
- * @param {Object} object
- * @returns {Objects}
+ * @param {object} object
+ * @returns {object}
  */
 function clone(object) {
-	let copy
+  let copy
 
-	if (isNull(object) || !isObject(object)) {
-		return object
-	}
+  if (isNull(object) || !isObject(object)) {
+    return object
+  }
 
-	if (isArray(object)) {
-		const count = object.length
-		copy = []
+  if (isArray(object)) {
+    const count = object.length
+    copy = []
 
-		for (let i = 0; i < count; i++) {
-			copy[i] = clone(object[i])
-		}
+    for (let i = 0; i < count; i++) {
+      copy[i] = clone(object[i])
+    }
 
-		return copy
-	} else {
-		copy = {}
+    return copy
+  } else {
+    copy = {}
 
-		for (let key in object) {
-			if (object.hasOwnProperty(key)) {
-				if (!isObject(object[key])) {
-					copy[key] = object[key]
-				} else {
-					copy[key] = clone(object[key])
-				}
-			}
-		}
-	}
+    for (let key in object) {
+      if (object.hasOwnProperty(key)) {
+        if (!isObject(object[key])) {
+          copy[key] = object[key]
+        } else {
+          copy[key] = clone(object[key])
+        }
+      }
+    }
+  }
 
-	return copy
+  return copy
 }
 
 module.exports = clone
